d637bb3d588872e376206b53bd50b72e73664af6
GitOfK <1850402105@qq.com>
2023-09-17 11:41:39 +0800

    2023年9月17日 11点41分

--- style-focus.js
+++ style-focus.js
@@ -10,7 +10,51 @@ videoContainerEl.style.height = "80vh";
 const frameContainerEl = document.querySelector("#__next > div");
 console.log(frameContainerEl);
 frameContainerEl.style.height = "80vh";
+/*
+ * ********
+ * */
 
 const iframeEl = document.querySelector("iframe");
 console.log(iframeEl);
 iframeEl.style.height = "80vh";
+
+const courseMainbarEl = document.querySelector(
+  ".course-mainbar.lecture-content",
+);
+console.log(courseMainbarEl);
+courseMainbarEl.style.display = "flex";
+courseMainbarEl.style.flexDirection = "column";
+
+const lectureHeadingEl = document.querySelector("#lecture_heading");
+console.log(lectureHeadingEl);
+lectureHeadingEl.style.order = "1";
+
+const mySubtitlesContainerEl = document.createElement("div");
+mySubtitlesContainerEl.id = "my-subtitles-container";
+mySubtitlesContainerEl.style.background = "rgba(0, 0, 0, 0.3)";
+mySubtitlesContainerEl.style.borderRadius = ".2em";
+mySubtitlesContainerEl.style.display = "flex";
+mySubtitlesContainerEl.style.flexDirection = "column-reverse";
+mySubtitlesContainerEl.style.fontSize = "2rem";
+mySubtitlesContainerEl.style.minHeight = "8em";
+mySubtitlesContainerEl.style.padding = ".2em .4em";
+
+const DUMMY_SUBTITLE_TEXT = "Dummy subtitle text";
+const mySubtitleEl = document.createElement("p");
+mySubtitleEl.id = "my-subtitle";
+mySubtitleEl.textContent = DUMMY_SUBTITLE_TEXT;
+mySubtitleEl.style.color = "#fffddd";
+
+mySubtitlesContainerEl.appendChild(mySubtitleEl);
+
+const insertTargetEl = document.querySelector("#__next");
+insertTargetEl.insertBefore(mySubtitlesContainerEl, insertTargetEl.firstChild);
+
+setInterval(() => {
+  const subtitleEl = document.querySelector('[data-testid="subtitle-text"]');
+  const nowSubtitleText = subtitleEl.textContent;
+  const oldSubtitleText = mySubtitleEl.textContent;
+  if (nowSubtitleText !== oldSubtitleText) {
+    mySubtitleEl.textContent = nowSubtitleText;
+  }
+}, 1000);
--- style-header.js
+++ style-header.js
@@ -1,21 +1,9 @@
-function styleHeader() {
-  const pageHeight = window.innerHeight;
+function styleHeaderMain() {
   const headerEl = document.querySelector(".full-width.half-height");
-  const headerHeight = headerEl.offsetHeight;
+
   headerEl.style.bottom = "0";
+  headerEl.style.top = "initial";
   headerEl.style.position = "absolute";
-  headerEl.style.height = `${headerHeight}px`;
-  headerEl.style.top = `${pageHeight - headerHeight}px`;
 }
 
-setInterval(() => {
-  styleHeader();
-}, 1000);
-
-{
-  const headerEl = document.querySelector(".full-width.half-height");
-  const headerRect = headerEl.getBoundingClientRect();
-  const pageHeight = window.innerHeight;
-  const distanceToBottom = pageHeight - headerRect.bottom;
-  console.log(distanceToBottom);
-}
+styleHeaderMain();
